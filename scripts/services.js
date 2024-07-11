dcd67e1c06e7ba7c2a35ec81975f86a25c334445
RoyDMAP <roy.dimapilis@sdgku.edu>
2024-07-10 17:12:51 -0700

    creating pet salon

--- scripts/services.js
+++ scripts/services.js
@@ -1,15 +1,83 @@
-function Service(description, price){
-    this.description = description;
-    this.price = price
+console.log("hello")
+function Services(Services, price){
+    this.Services = Services;
+    this.price = price;
 }
 
-function register(){
-    let inputDescription = $("#txtDescription").val();
+function Services(){
+    let inputServices = $("#txtServices").val();
+    let newServices = new Services(inputDescription, inputPrice)
 
-    //get inputPrice
-
-    //create a new Obj
-    let newService = new Service(inputDescription, inputPrice)
-
-    // Do validations
 }
+console.log("Services.js")
+
+let Services = []
+ 
+function newServices(Services, price){
+    this.Services=Services;   
+    this.price=price;
+    
+}
+
+function isValid(Services){
+    let validation=true;
+
+    if(newServices.name==""){ 
+        
+        alert("Please add a description");
+        validation=false;
+}
+    return validation;
+
+}
+
+function Services(){
+    let inputServices=document.getElementById("txtServices").value;
+    let inputPrice=document.getElementById("txtPrice").value;
+    
+
+    console.log(inputServices,inputPrice);
+
+let newServices = new Services(inputServices,inputPrice); //add Service and Type
+
+if(isValid(newServices)){
+    
+    Services.push(newServices);
+    console.log(newServices); 
+    displayServices(); 
+    displayTable();
+    clearForm();
+    }
+}
+function clearForm(){
+    $("#txtServices").value="";
+    $("#txtPrice").value="";
+    //document.getElementById("txtServices").value="";
+    //document.getElementById("txtPrice").value="";
+
+}
+
+function calculateAverage(){}
+
+function deleteServices(aServices){
+    console.log("Deleting Services ... " + aServices);
+    document.getElementById(aServices).remove();
+    Services.splice(aServices,1);
+    displayTable();
+    displayServicesCount();
+}
+function init(){
+    //the initialization function
+    let Services1 = new Services("Grooming","Price");
+    let Services2 = new Services("Nail Clipping","Price");
+    let Services3 = new Services("Ear Cleaning","Price");
+    Services.push(Services1,Services2,Services3);
+    displayServicesCount();
+    displayTable();
+
+}
+window.onload=init;//waiting for render the HTML
+
+
+
+
